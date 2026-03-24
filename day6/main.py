7924b85997b244f178dda3eeb6403feec9b5f72c
soactualy <allysa.ribo@gmail.com>
2026-03-24 22:51:03 +0800

    added additional projects
    day 5 - password generator
    day 6 - functions
    day 7 - hangman
    day 8 - caesar cypher
    day 9 - blind auction project
    day 10 - calculator project

---
+++ day6/main.py
@@ -0,0 +1,11 @@
+word = input("What word would you like to guess?\n")
+length_word = len(word)
+print("This is your word : ")
+word_board = ['_' for i in range(length_word)]
+letter_guess = input("Would you like to guess a letter\n")
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# find position in
+
+print(position)
